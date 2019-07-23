ec27c39bc14866fe64ac9bf1183b26549c0a1d82
Taylor Reid <trgit@hoghill.net>
2019-07-23 12:08:46 -0700

    use Function instead of eval in patchClass

--- lessfog.js
+++ lessfog.js
@@ -8,16 +8,17 @@
  * @returns {Class}              Revised class
  */
 function patchClass(klass, func, line_number, line, new_line) {
-    let funcStr = func.toString()
-    let lines = funcStr.split("\n")
-    if (lines[line_number].trim() == line.trim()) {
-        lines[line_number] = lines[line_number].replace(line, new_line);
+  let funcStr = func.toString()
+  let lines = funcStr.split("\n")
+  if (lines[line_number].trim() == line.trim()) {
+    lines[line_number] = lines[line_number].replace(line, new_line);
     classStr = klass.toString()
     fixedClass = classStr.replace(funcStr, lines.join("\n"))
-    return  eval ("(" + fixedClass + ")")
-    } else {
-        console.log("Function has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr)
-    }
+    return Function('"use strict";return (' + fixedClass + ')')();
+  }
+  else {
+    console.log("Function has wrong content at line ", line_number, " : ", lines[line_number].trim(), " != ", line.trim(), "\n", funcStr)
+  }
 }
 
 /**
