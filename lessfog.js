453fc0bcb6a46eba6e5ebb655f45cd03f19a5d35
Taylor Reid <trgit@hoghill.net>
2019-08-19 08:48:37 -0700

    update for FVTT 0.3.4

--- lessfog.js
+++ lessfog.js
@@ -22,21 +22,24 @@ function patchClass(klass, func, line_number, line, new_line) {
 }
 
 /**
- * Patch SightLayer class to allow GM to see through the FOW and to see all tokens on the canvas
+ * Patch the SightLayer class to tweak the FOW transparency for the GM,
+ * and to let the GM see all tokens on the canvas
  */
 function patchSightLayerClass() {
-    newClass = patchClass(SightLayer, SightLayer.prototype.draw, 8,
-      "unexplored: 1.0,",
-      "unexplored: game.user.isGM ? ( this.fogExploration ? 0.5 : 0.0 ) : 1.0,");
+
+    newClass = patchClass(SightLayer, SightLayer.prototype.draw, 9,
+      `if ( game.user.isGM ) this.alphas.unexplored = 0.7;`,
+      `this.alphas.unexplored = game.user.isGM ? ( this.fogExploration ? 0.5 : 0.0 ) : 1.0;`);
     if (!newClass) return;
-    newClass = patchClass(newClass, newClass.prototype.draw, 9,
-      "dark: this.fogExploration ? 0.75 : 1.0,",
-      "dark: ( this.fogExploration || game.user.isGM ) ? 0.7 : 1.0,");
+    newClass = patchClass(newClass, newClass.prototype.draw, 10,
+      `if ( !this.fogExploration ) this.alphas.dark = this.alphas.unexplored;`,
+      `this.alphas.dark = ( this.fogExploration || game.user.isGM ) ? 0.7 : 1.0;
+      this.alphas.dim = 0.4;`);
     if (!newClass) return;
     newClass = patchClass(newClass, SightLayer.prototype.restrictVisibility, 2,
       "// Tokens",
-      `if ( !game.user.isGM )
-       // Tokens`);
+      `// Tokens
+      if ( !game.user.isGM )`);
     if (!newClass) return;
     SightLayer = newClass
 }
