6fcfb76235ec714f790b3f28730facab72c394c6
Taylor Reid <trgit@hoghill.net>
2019-08-21 17:03:08 -0700

    tweak values and add blur to bright areas

--- lessfog.js
+++ lessfog.js
@@ -23,19 +23,32 @@ function patchClass(klass, func, line_number, line, new_line) {
 
 /**
  * Patch the SightLayer class to tweak the FOW transparency for the GM,
- * and to let the GM see all tokens on the canvas
+ * optimize contrast between dim, dark, and unexplored areas, blend dim
+ * and bright areas, and let the GM see all tokens on the canvas.
  */
 function patchSightLayerClass() {
 
+// Hide unexplored areas from players at all times.
+// GM view is half shaded when fogExploration is enabled.
     newClass = patchClass(SightLayer, SightLayer.prototype.draw, 9,
       `if ( game.user.isGM ) this.alphas.unexplored = 0.7;`,
       `this.alphas.unexplored = game.user.isGM ? ( this.fogExploration ? 0.5 : 0.0 ) : 1.0;`);
     if (!newClass) return;
+// Reveal previously explored dark areas if fogExploration is true. 
+// GM shading calibrated to match setting for unexplored areas.
+// dark = 1 - ((1 - 0.6) * 0.5) if fogExploration is false.
     newClass = patchClass(newClass, newClass.prototype.draw, 10,
       `if ( !this.fogExploration ) this.alphas.dark = this.alphas.unexplored;`,
-      `this.alphas.dark = ( this.fogExploration || game.user.isGM ) ? 0.7 : 1.0;
-      this.alphas.dim = 0.4;`);
+      `this.alphas.dark = this.fogExploration ? 0.6 : ( game.user.isGM  ? 0.8 : 1.0 );
+      this.alphas.dim = 0.3;`);
     if (!newClass) return;
+// Heavily blur edge between bright and dim areas.
+    newClass = patchClass(newClass, SightLayer.prototype._drawShadowMap, 6,
+      `source.mask = source.fov;`,
+      `source.mask = source.fov;
+      if (hex == this.queues.bright.hex) source.filters = game.settings.get("core", "softShadows") ? [new PIXI.filters.BlurFilter(15)] : null;`);
+    if (!newClass) return;
+// Reveal all tokens to the GM. 
     newClass = patchClass(newClass, SightLayer.prototype.restrictVisibility, 2,
       "// Tokens",
       `// Tokens
