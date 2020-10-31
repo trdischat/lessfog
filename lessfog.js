f096e7656acc770a70fd6202a40005b0d8d1afea
tsrgit <trgit@hoghill.net>
2020-10-31 12:05:45 -0700

    revert to version for fvtt 0.6.6

--- lessfog.js
+++ lessfog.js
@@ -11,34 +11,27 @@ Hooks.once('init', async function () {
     // Register custom module settings
     registerSettings();
 
-    if (isNewerVersion('0.7.3', game.data.version)) {
-        /**
-         * Adjust the FOW transparency for the GM, and optimize contrast
-         * between bright, dim, dark, explored, and unexplored areas.
-         */
-        SightLayer.prototype._configureChannels = function () {
-            // Set up the default channel order and alphas (with no darkness)
-            const channels = {
-                black: { alpha: game.user.isGM ? game.settings.get("lessfog", "alpha_unexplored") : 1.0 },   // ! Changed line
-                explored: { alpha: game.settings.get("lessfog", "alpha_explored") },                         // ! Changed line
-                dark: { alpha: game.settings.get("lessfog", "alpha_dark") },                                 // ! Changed line
-                dim: { alpha: game.settings.get("lessfog", "alpha_dim") },                                   // ! Changed line
-                bright: { alpha: 0.0 }
-            };
+    /**
+     * Adjust the FOW transparency for the GM, and optimize contrast
+     * between bright, dim, dark, explored, and unexplored areas.
+     */
+    SightLayer.prototype._configureChannels = function () {
+        // Set up the default channel order and alphas (with no darkness)
+        const channels = {
+            black: { alpha: game.user.isGM ? game.settings.get("lessfog", "alpha_unexplored") : 1.0 },   // ! Changed line
+            explored: { alpha: game.settings.get("lessfog", "alpha_explored") },                         // ! Changed line
+            dark: { alpha: game.settings.get("lessfog", "alpha_dark") },                                 // ! Changed line
+            dim: { alpha: game.settings.get("lessfog", "alpha_dim") },                                   // ! Changed line
+            bright: { alpha: 0.0 }
+        };
 
-            // Modify alpha levels for darkness value and compute hex code
-            for (let c of Object.values(channels)) {
-                c.hex = PIXI.utils.rgb2hex([c.alpha, c.alpha, c.alpha]);
-            }
-            return channels;
+        // Modify alpha levels for darkness value and compute hex code
+        for (let c of Object.values(channels)) {
+            c.hex = PIXI.utils.rgb2hex([c.alpha, c.alpha, c.alpha]);
         }
-    } else {
-        CONFIG.Canvas.blurStrength = 16;
-        CONFIG.Canvas.darknessColor = game.settings.get("lessfog", "color_dark") ? game.settings.get("lessfog", "color_dark") : 0x242448;
-        CONFIG.Canvas.exploredColor = game.settings.get("lessfog", "color_explored") ? game.settings.get("lessfog", "color_explored") : 0x7f7f7f;
-        // CONFIG.Canvas.unexploredColor = game.settings.get("lessfog", "color_unexplored") ? game.settings.get("lessfog", "color_unexplored") : 0x000000;
-        CONFIG.Canvas.lightLevels.dim = game.settings.get("lessfog", "level_dim");
+        return channels;
     }
+
 });
 
 /* ------------------------------------ */
@@ -53,41 +46,19 @@ Hooks.once('ready', function () {
         `t.visible = ( !this.tokenVision && !t.data.hidden ) || ( game.settings.get("lessfog", "reveal_tokens") && game.user.isGM ) || t.isVisible;`);
     if (!newClass) return;
     SightLayer.prototype.restrictVisibility = newClass.prototype.restrictVisibility;
-    if (isNewerVersion('0.7.3', game.data.version)) {
-        // Change scaling of soft shadow blur.
-        newClass = Canvas;
-        newClass = patchMethod(newClass, "pan", 12,
-            `this.sight.blurDistance = 20 / (CONFIG.Canvas.maxZoom - Math.round(constrained.scale) + 1);`,
-            `this.sight.blurDistance = 12 * constrained.scale;`);
-        if (!newClass) return;
-        Canvas.prototype.pan = newClass.prototype.pan;
-        // Increase blur on light tinting.
-        newClass = LightingLayer;
-        newClass = patchMethod(newClass, "_drawLightingContainer", 13,
-            `const bf = new PIXI.filters.BlurFilter(16);`,
-            `const bf = new PIXI.filters.BlurFilter(32);`);
-        if (!newClass) return;
-        LightingLayer.prototype._drawLightingContainer = newClass.prototype._drawLightingContainer;
-    // } else {
-    //     // Adjust the FOW transparency for the GM.
-    //     newClass = SightLayer;
-    //     newClass = patchMethod(newClass, "_drawFogContainer", 7,
-    //         `fog.unexplored.beginFill(CONFIG.Canvas.unexploredColor, 1.0).drawShape(r).endFill();`,
-    //         `let dRGB = hexToRGB(CONFIG.Canvas.darknessColor);
-    //         let uRGB = hexToRGB(game.user.isGM ? CONFIG.Canvas.unexploredColor : 0x000000);
-    //         let uColor = rgbToHex(dRGB.map((c, i) => uRGB[i] / ((1-canvas.scene.data.darkness) + (canvas.scene.data.darkness*c))));
-    //         fog.unexplored.beginFill(uColor, 1.0).drawShape(r).endFill();`);
-    //     if (!newClass) return;
-    //     SightLayer.prototype._drawFogContainer = newClass.prototype._drawFogContainer;
-    //     // Compensate for darkness setting in explored areas.
-    //     newClass = SightLayer;
-    //     newClass = patchMethod(newClass, "refresh", 10,
-    //         `prior.fov.tint = CONFIG.Canvas.exploredColor;`,
-    //         `let dRGB = hexToRGB(CONFIG.Canvas.darknessColor);
-    //         let eRGB = hexToRGB(CONFIG.Canvas.exploredColor);
-    //         prior.fov.tint = rgbToHex(dRGB.map((c, i) => eRGB[i] / ((1-canvas.scene.data.darkness) + (canvas.scene.data.darkness*c))));`);
-    //     if (!newClass) return;
-    //     SightLayer.prototype.refresh = newClass.prototype.refresh;
-    }
+    // Change scaling of soft shadow blur.
+    newClass = Canvas;
+    newClass = patchMethod(newClass, "pan", 12,
+        `this.sight.blurDistance = 20 / (CONFIG.Canvas.maxZoom - Math.round(constrained.scale) + 1);`,
+        `this.sight.blurDistance = 12 * constrained.scale;`);
+    if (!newClass) return;
+    Canvas.prototype.pan = newClass.prototype.pan;
+    // Increase blur on light tinting.
+    newClass = LightingLayer;
+    newClass = patchMethod(newClass, "_drawLightingContainer", 13,
+        `const bf = new PIXI.filters.BlurFilter(16);`,
+        `const bf = new PIXI.filters.BlurFilter(32);`);
+    if (!newClass) return;
+    LightingLayer.prototype._drawLightingContainer = newClass.prototype._drawLightingContainer;
 
 });
--- module/settings.js
+++ module/settings.js
@@ -2,86 +2,42 @@ export const registerSettings = function () {
     /**
      * Configuration for dim, dark, explored, and unexplored settings.
      */
-    if (isNewerVersion('0.7.3', game.data.version)) {
-        game.settings.register("lessfog", "alpha_unexplored", {
-            name: "Unexplored",
-            hint: "Darkness in unexplored areas (GM only)",
-            scope: "world",
-            type: Number,
-            default: 0.85,
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        game.settings.register("lessfog", "alpha_explored", {
-            name: "Explored",
-            hint: "Darkness in explored areas",
-            scope: "world",
-            type: Number,
-            default: 0.75,
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        game.settings.register("lessfog", "alpha_dark", {
-            name: "Dark",
-            hint: "Darkness in dark areas (not used)",
-            scope: "world",
-            type: Number,
-            default: 0.6,
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        game.settings.register("lessfog", "alpha_dim", {
-            name: "Dim",
-            hint: "Darkness in dim areas",
-            scope: "world",
-            type: Number,
-            default: 0.4,
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-    } else {
-        game.settings.register("lessfog", "level_dim", {
-            name: "Dimming",
-            hint: "Relative brightness in dim areas",
-            scope: "world",
-            type: Number,
-            range: {
-                min: 0,
-                max: 1,
-                step: 0.05
-            },
-            default: 0.5,
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        game.settings.register("lessfog", "color_dark", {
-            name: "Color - Darkness",
-            hint: "Input color of dark areas in hex format: 0xRRGGBB",
-            scope: "world",
-            type: String,
-            default: "0x242448",
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        game.settings.register("lessfog", "color_explored", {
-            name: "Color - Explored",
-            hint: "Input color of explored areas in hex format: 0xRRGGBB",
-            scope: "world",
-            type: String,
-            default: "0x7f7f7f",
-            config: true,
-            onChange: s => { canvas.draw(); }
-        });
-        // game.settings.register("lessfog", "color_unexplored", {
-        //     name: "Color - Unexplored (GM only)",
-        //     hint: "Input color of unexplored areas in hex format: 0xRRGGBB",
-        //     scope: "world",
-        //     type: String,
-        //     default: "0x202020",
-        //     config: true,
-        //     onChange: s => { canvas.draw(); }
-        // });
-    }
+    game.settings.register("lessfog", "alpha_unexplored", {
+        name: "Unexplored",
+        hint: "Darkness in unexplored areas (GM only)",
+        scope: "world",
+        type: Number,
+        default: 0.85,
+        config: true,
+        onChange: s => { canvas.draw(); }
+    });
+    game.settings.register("lessfog", "alpha_explored", {
+        name: "Explored",
+        hint: "Darkness in explored areas",
+        scope: "world",
+        type: Number,
+        default: 0.75,
+        config: true,
+        onChange: s => { canvas.draw(); }
+    });
+    game.settings.register("lessfog", "alpha_dark", {
+        name: "Dark",
+        hint: "Darkness in dark areas (not used)",
+        scope: "world",
+        type: Number,
+        default: 0.6,
+        config: true,
+        onChange: s => { canvas.draw(); }
+    });
+    game.settings.register("lessfog", "alpha_dim", {
+        name: "Dim",
+        hint: "Darkness in dim areas",
+        scope: "world",
+        type: Number,
+        default: 0.4,
+        config: true,
+        onChange: s => { canvas.draw(); }
+    });
     /**
      * Option to reveal tokens to the GM.
      */
