4ae409baf0d725d3b4b2f8473b253cc27071380a
tsrgit <trgit@hoghill.net>
2020-10-31 13:42:20 -0700

    rewrite module for FVTT 0.7.x

--- README.md
+++ README.md
@@ -11,14 +11,7 @@ In the GM view below, the `lessfog` module reveals (to the GM only) any unexplor
 
 ![GM view](gm.jpg)
 
-# Installation
-See https://github.com/foundry-vtt-community/wiki/wiki/Modules#installing-modules. Open the Add-on Modules tab in the Configuration and Setup dialog. Click Install Module, paste `https://raw.githubusercontent.com/trdischat/lessfog/master/module.json` in as the Manifest URL, then click Install.
-
-As DM go to the `Manage Modules` options menu in the Game Settings for your World, then enable the `Less Fog` module.
-
-This module requires FVTT version 0.6.1 or later.
-
 # License
-This Foundry VTT module, written by trdischat with major assistance from KaKaRoTo, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
+This Foundry VTT module, written by trdischat with major assistance from Calego and KaKaRoTo, is licensed under a [Creative Commons Attribution 4.0 International License](http://creativecommons.org/licenses/by/4.0/).
 
 This work is licensed under Foundry Virtual Tabletop [EULA - Limited License Agreement for module development v 0.1.6](http://foundryvtt.com/pages/license.html).
--- lessfog.js
+++ lessfog.js
@@ -2,6 +2,23 @@
 import { registerSettings } from './module/settings.js';
 import { patchMethod } from './module/patchlib.js';
 
+/**
+ * Set unexplored fog alpha for GMs only.
+ * Setting the color should be sufficient but is bugged.
+ * Issue: https://gitlab.com/foundrynet/foundryvtt/-/issues/3955
+ * @param {number} unexploredDarkness - number between 0 and 1
+ */
+export function setUnexploredForGM(unexploredDarkness) {
+    if (game.user.isGM) {
+        // canvas.sight.refresh() should be applying the new color but isn't - https://gitlab.com/foundrynet/foundryvtt/-/issues/3955
+        // CONFIG.Canvas.unexploredColor = PIXI.utils.rgb2hex([1-unexploredDarkness, 1-unexploredDarkness, 1-unexploredDarkness]);
+        // canvas.sight.refresh();
+
+        // instead we set the alpha of the `unexplored` container
+        canvas.sight.fog.unexplored.alpha = unexploredDarkness;
+    }
+}
+
 /* ------------------------------------ */
 /* Initialize module					*/
 /* ------------------------------------ */
@@ -11,34 +28,16 @@ Hooks.once('init', async function () {
     // Register custom module settings
     registerSettings();
 
-    if (isNewerVersion('0.7.3', game.data.version)) {
-        /**
-         * Adjust the FOW transparency for the GM, and optimize contrast
-         * between bright, dim, dark, explored, and unexplored areas.
-         */
-        SightLayer.prototype._configureChannels = function () {
-            // Set up the default channel order and alphas (with no darkness)
-            const channels = {
-                black: { alpha: game.user.isGM ? game.settings.get("lessfog", "alpha_unexplored") : 1.0 },   // ! Changed line
-                explored: { alpha: game.settings.get("lessfog", "alpha_explored") },                         // ! Changed line
-                dark: { alpha: game.settings.get("lessfog", "alpha_dark") },                                 // ! Changed line
-                dim: { alpha: game.settings.get("lessfog", "alpha_dim") },                                   // ! Changed line
-                bright: { alpha: 0.0 }
-            };
+    /**
+     * Adjust the dim light level and the explored darkness color
+     */
 
-            // Modify alpha levels for darkness value and compute hex code
-            for (let c of Object.values(channels)) {
-                c.hex = PIXI.utils.rgb2hex([c.alpha, c.alpha, c.alpha]);
-            }
-            return channels;
-        }
-    } else {
-        CONFIG.Canvas.blurStrength = 16;
-        CONFIG.Canvas.darknessColor = game.settings.get("lessfog", "color_dark") ? game.settings.get("lessfog", "color_dark") : 0x242448;
-        CONFIG.Canvas.exploredColor = game.settings.get("lessfog", "color_explored") ? game.settings.get("lessfog", "color_explored") : 0x7f7f7f;
-        // CONFIG.Canvas.unexploredColor = game.settings.get("lessfog", "color_unexplored") ? game.settings.get("lessfog", "color_unexplored") : 0x000000;
-        CONFIG.Canvas.lightLevels.dim = game.settings.get("lessfog", "level_dim");
-    }
+    // set the dim light level
+    CONFIG.Canvas.lightLevels.dim = 1 - game.settings.get("lessfog", "dim_darkness");
+
+    // set the explored color based on selected darkness level
+    const exploredDarkness = 1 - game.settings.get("lessfog", "explored_darkness");
+    CONFIG.Canvas.exploredColor = PIXI.utils.rgb2hex([exploredDarkness, exploredDarkness, exploredDarkness]);
 });
 
 /* ------------------------------------ */
@@ -46,48 +45,22 @@ Hooks.once('init', async function () {
 /* ------------------------------------ */
 Hooks.once('ready', function () {
 
-    // Allow the GM to see all tokens.
+    /**
+     * Patch `restrictVisibility` to allow the GM to see all tokens.
+     */
     let newClass = SightLayer;
     newClass = patchMethod(newClass, "restrictVisibility", 4,
         `t.visible = ( !this.tokenVision && !t.data.hidden ) || t.isVisible;`,
         `t.visible = ( !this.tokenVision && !t.data.hidden ) || ( game.settings.get("lessfog", "reveal_tokens") && game.user.isGM ) || t.isVisible;`);
     if (!newClass) return;
     SightLayer.prototype.restrictVisibility = newClass.prototype.restrictVisibility;
-    if (isNewerVersion('0.7.3', game.data.version)) {
-        // Change scaling of soft shadow blur.
-        newClass = Canvas;
-        newClass = patchMethod(newClass, "pan", 12,
-            `this.sight.blurDistance = 20 / (CONFIG.Canvas.maxZoom - Math.round(constrained.scale) + 1);`,
-            `this.sight.blurDistance = 12 * constrained.scale;`);
-        if (!newClass) return;
-        Canvas.prototype.pan = newClass.prototype.pan;
-        // Increase blur on light tinting.
-        newClass = LightingLayer;
-        newClass = patchMethod(newClass, "_drawLightingContainer", 13,
-            `const bf = new PIXI.filters.BlurFilter(16);`,
-            `const bf = new PIXI.filters.BlurFilter(32);`);
-        if (!newClass) return;
-        LightingLayer.prototype._drawLightingContainer = newClass.prototype._drawLightingContainer;
-    // } else {
-    //     // Adjust the FOW transparency for the GM.
-    //     newClass = SightLayer;
-    //     newClass = patchMethod(newClass, "_drawFogContainer", 7,
-    //         `fog.unexplored.beginFill(CONFIG.Canvas.unexploredColor, 1.0).drawShape(r).endFill();`,
-    //         `let dRGB = hexToRGB(CONFIG.Canvas.darknessColor);
-    //         let uRGB = hexToRGB(game.user.isGM ? CONFIG.Canvas.unexploredColor : 0x000000);
-    //         let uColor = rgbToHex(dRGB.map((c, i) => uRGB[i] / ((1-canvas.scene.data.darkness) + (canvas.scene.data.darkness*c))));
-    //         fog.unexplored.beginFill(uColor, 1.0).drawShape(r).endFill();`);
-    //     if (!newClass) return;
-    //     SightLayer.prototype._drawFogContainer = newClass.prototype._drawFogContainer;
-    //     // Compensate for darkness setting in explored areas.
-    //     newClass = SightLayer;
-    //     newClass = patchMethod(newClass, "refresh", 10,
-    //         `prior.fov.tint = CONFIG.Canvas.exploredColor;`,
-    //         `let dRGB = hexToRGB(CONFIG.Canvas.darknessColor);
-    //         let eRGB = hexToRGB(CONFIG.Canvas.exploredColor);
-    //         prior.fov.tint = rgbToHex(dRGB.map((c, i) => eRGB[i] / ((1-canvas.scene.data.darkness) + (canvas.scene.data.darkness*c))));`);
-    //     if (!newClass) return;
-    //     SightLayer.prototype.refresh = newClass.prototype.refresh;
-    }
 
 });
+
+/* ------------------------------------ */
+/* When Canvas is ready					*/
+/* ------------------------------------ */
+Hooks.once('canvasReady', function () {
+    const unexploredDarkness = game.settings.get("lessfog", "unexplored_darkness");
+    setUnexploredForGM(unexploredDarkness);
+})
