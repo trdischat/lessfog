849257230ab852cfa7a93b99cf82ebcb40022813
Taylor Reid <trgit@hoghill.net>
2021-06-25 00:58:06 -0700

    update canvas refresh for FVTT 0.8.2

--- lessfog.js
+++ lessfog.js
@@ -12,9 +12,12 @@ export function setUnexploredForPermitted(unexploredDarkness) {
     if (game.user.isGM || game.settings.get("lessfog", "affect_all")) {
         if (isNewerVersion('0.7.6', game.data.version)) {
             canvas.sight.fog.unexplored.alpha = unexploredDarkness;
-        } else {
+        } else if (isNewerVersion('0.8.2', game.data.version)) {
             CONFIG.Canvas.unexploredColor = PIXI.utils.rgb2hex([1 - unexploredDarkness, 1 - unexploredDarkness, 1 - unexploredDarkness]);
             canvas.sight.refresh();
+        } else {
+            CONFIG.Canvas.unexploredColor = PIXI.utils.rgb2hex([1 - unexploredDarkness, 1 - unexploredDarkness, 1 - unexploredDarkness]);
+            canvas.perception.schedule({sight: {refresh: true}});
         }
     }
 }
